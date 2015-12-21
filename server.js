0475e07e2956b50738cd4926ea39249cc6d0f44c
Jesse Zelaya <jessezelaya1@gmail.com>
2015-12-20 23:10:48 -0800

    Set up: Base Set up, DB connection, Configuration and Server

--- server.js
+++ server.js
@@ -2,9 +2,22 @@
 // ==================================================
 var express    = require('express');//Get express
 var app        = express();//Create app with express
-var morgan     = require('morgan');
-var bodyParser = require('body-parser');
-var port       = process.env.PORT || 3000;
+var mongoose   = require('mongoose');//Use with Mongo Database
+var morgan     = require('morgan');//Log all requests to the console
+var bodyParser = require('body-parser');//Grab info from POST requests
+var config     = require('./config');
+
+
+// DATABASE CONNECTION
+// ==================================================
+//Connect to Database
+mongoose.connect(config.database);
+// Test Database Connection
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Could not establish database connection'));
+db.once('open', function(){
+  console.log("Successufl database connection");
+})
 
 
 // CONFIGURATION
@@ -14,7 +27,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 
+// Log all API requests to the console
+app.use(morgan('dev'));
+
+
 // START SERVER
 // ==================================================
-app.listen(port);
-console.log('Listening on PORT: ', port);
+app.listen(config.port);
+console.log('Listening on PORT: ', config.port);
