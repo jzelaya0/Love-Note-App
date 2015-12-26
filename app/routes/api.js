8c1969faf23407ff6d4ecdead4415f574b05d6b4
Jesse Zelaya <jessezelaya1@gmail.com>
2015-12-26 11:26:46 -0800

    Added POST route to create & save user in database

--- app/routes/api.js
+++ app/routes/api.js
@@ -2,11 +2,39 @@
 var bodyParser = require('body-parser');
 var User       = require('../models/user');
 
+
 module.exports = function(app,express){
   //Create new instance of Express router
   var apiRouter = express.Router();
 
-  // MIDDLEWARE
+  // API USER - CREATE A NEW USER
+  // ==================================================
+  // Route ending with /users
+  apiRouter.post('/users', function(req,res){
+    // Create a new user instance from the User Model
+    var user = new User();
+
+    // Set user's information
+    user.name     = req.body.name;
+    user.username = req.body.username;
+    user.email    = req.body.email;
+    user.password = req.body.password;
+
+    //Save the new user and check for errors
+    user.save(function(err){
+     if(err){
+       //A duplicate username or password was inputted
+       if(err.code == 11000){
+         return res.json({success: false, message: 'A user with that username or email exists'});
+       }else {
+         return res.send(err);
+       }
+     }
+     res.json({message: 'User successfully created!'});
+   });//End save
+  });
+
+// MIDDLEWARE
   // ==================================================
   apiRouter.use(function(req,res,next){
     console.log('A visitor has arrived');
