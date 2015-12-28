747e2583acfdfd2923b0fe6552894e9c28b02bd1
Jesse Zelaya <jessezelaya1@gmail.com>
2015-12-28 13:38:26 -0800

    Set up User Route - GET all users

--- app/routes/api.js
+++ app/routes/api.js
@@ -38,7 +38,9 @@ module.exports = function(app,express){
    });//End save
   });
 
-// MIDDLEWARE
+
+
+  // MIDDLEWARE
   // ==================================================
   apiRouter.use(function(req,res,next){
     console.log('A visitor has arrived');
@@ -51,6 +53,24 @@ module.exports = function(app,express){
   apiRouter.get('/', function(req,res){
     res.json({message:'Welcome to the Love Note API!'});
   });
+
+
+  // API USER ROUTES
+  // ==================================================
+  // Routes ending in - /users
+  apiRouter.route('/users')
+      //GET - all users at /api/users
+      .get(function(req,res){
+        User.find(function(err,users){
+          //Send errors if any
+          if(err)
+            res.send(err);
+          //Respond with all users
+          res.json(users);
+        });
+      })//end get all users
+
+      
 
   return apiRouter;
 };//End module exports
