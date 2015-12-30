8b9c2330967fb1387503bed6f9216e59e9ce94e1
Jesse Zelaya <jessezelaya1@gmail.com>
2015-12-29 17:07:16 -0800

    Created Notes schema and model

---
+++ app/models/note.js
@@ -0,0 +1,22 @@
+// Note MODEL
+
+var mongoose = require('mongoose');
+var Schema   = mongoose.Schema;
+
+
+// Note SCHEMA
+// ==================================================
+var NoteSchema = new Schema({
+  title: String,
+  body: String,
+  category: String,
+  date_created: {type: Date, default: Date.now},
+  created_by: {type: Schema.ObjectId, ref: 'User'}//Reference note's creator
+});
+
+
+//Create a note model from schema
+var Note = mongoose.Model('Note', NoteSchema);
+
+//Export the note model
+module.exports = Note;
