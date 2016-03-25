cc9314a02d651d04d0b9234cbcc62f825ada34a8
Jesse Zelaya <jessezelaya1@gmail.com>
2016-03-24 23:08:49 -0700

    Added task to watch changes for main angular file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,25 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
 
-gulp.task('default', function(){
-  console.log("Gulp is running!");
+// PATHS
+// ====================================
+var output = {
+  angularApp : "public/app"
+};
+
+
+// DEFAULT TASK
+// ====================================
+gulp.task('default', ['watch']);
+
+
+gulp.task('angularApp', function(){
+  return gulp.src('src/app/app.js')
+    .pipe(uglify())
+    .pipe(gulp.dest(output.angularApp));
+});
+
+
+gulp.task('watch', function(){
+  gulp.watch('src/app/app.js', ["angularApp"]);
 })
