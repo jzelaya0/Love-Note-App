ac6809cd64c6fad73283161ca7d95b184e9c7432
Jesse Zelaya <jessezelaya1@gmail.com>
2016-03-27 23:23:33 -0700

    Set up task to for angular controllers

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
+var gulp       = require('gulp');
+var uglify     = require('gulp-uglify');
+var ngAnnotate = require('gulp-ng-annotate');
+var concat     = require('gulp-concat');
+var jshint     = require('gulp-jshint');
 
 // PATHS
 // ====================================
 var output = {
-  angularApp : "public/app"
+  angularApp : "public/app",
+  angularControllers: "public/app/controllers/"
 };
 
 
@@ -13,13 +17,28 @@ var output = {
 gulp.task('default', ['watch']);
 
 
+// Main Angluar app
 gulp.task('angularApp', function(){
   return gulp.src('src/app/app.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter())
+    .pipe(ngAnnotate())
     .pipe(uglify())
     .pipe(gulp.dest(output.angularApp));
 });
 
+// Angular controllers
+gulp.task('angularControllers', function(){
+  return gulp.src('src/app/controllers/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter())
+    .pipe(ngAnnotate())
+    .pipe(concat('controllers.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(output.angularControllers));
+});
 
 gulp.task('watch', function(){
   gulp.watch('src/app/app.js', ["angularApp"]);
-})
+  gulp.watch('src/app/controllers/*js', ["angularControllers"]);
+});
