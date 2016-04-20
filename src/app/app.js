ca682a375dcd66a63c3baf335eada9ba469d68ef
Jesse Zelaya <jessezelaya1@gmail.com>
2016-04-19 22:04:09 -0700

    Created Authetication Service for loging user in

--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,13 @@
 angular.module('loveNote', [
   'appRoutes',
   'mainCtrl'
-]);
+])
+
+
+// integrate tokens in requests
+.config(function ($httpProvider){
+
+  // attach auth interceptor from auth service to the http requests
+  $httpProvider.interceptor.push('AunthInterceptor');
+
+});
--- src/app/services/authService.js
+++ src/app/services/authService.js
@@ -1,1 +1,111 @@
-angular.moudle('authService', []);
+angular.module('authService', [])
+
+// ==============================
+// factory that handles token and
+// store them in local storage
+// ==============================
+  .factory('AuthToken', function($window){
+    // authToken object
+    var authTokenFactory = {};
+
+    // function to grab token from local storage
+    authTokenFactory.getToken = function () {
+      return $window.localStorage.getItem('token');
+    };
+
+    // function to set or clear token
+    // if token passed, then set it
+    // if no token, then clear it in local storage
+    authTokenFactory.setToken = function (token){
+      if(token){
+        $window.localStorage.setItem('token', token);
+      }else{
+        $window.localStorage.removeItem('token');
+      }
+    };
+
+    // return the token factory object
+    return authTokenFactory;
+
+  })
+
+  // ==============================
+  // factory to login user and get info
+  // ==============================
+
+  .factory('Auth', function ($http, $q, AuthToken){
+    // auth object
+    var authFactory = {};
+
+    // function to log user in
+    authFactory.login = function (username, password) {
+      // return promise object and its data
+      // post to the api's authenticate endpoint
+      return $http.post('/api/authenticate', {
+        email: email,
+        password: password
+      });
+    };
+
+    // function to log user out by clearing token
+    authFactory.logout = function (){
+      AuthToken.setToken();
+    };
+
+    // function to check if user is logged in
+    authFactory.isLoggedIn = function (){
+      if(AuthToken.getToken()){
+        return true;
+      }else {
+        return false;
+      }
+    };
+
+    // function to get logged in user
+    authFactory.getUser = function (){
+      if(AuthToken.getToken()) {
+        return $http.get('/api/me');
+      }else {
+        return $q.reject({message: "User does not have a token"});
+      }
+    };
+
+    // return auth factory object
+    return authTokenFactory;
+
+  })
+
+  // ==============================
+  // Factory to integrate token in http request
+  // ==============================
+  .factory('AunthInterceptor', function($q, $location, AuthToken){
+    // interceptor object
+    var interceptorFactory = {};
+
+    // function to execute on all http requests
+    interceptorFactory.request = function (config) {
+      //get token
+      var token = AuthToken.getToken();
+
+      // Add existing token to http request header as x-access-token
+      if (token) {
+        config.headers['x-access-token'] = token;
+      }
+
+      return config;
+    };
+
+    // function that happens on response errors
+    interceptorFactory.responseError = function (response) {
+      // if server responds with 404 error
+      if(response.status == 403) {
+        // redirect to the login page
+        $location.path('/login');
+      }
+
+      // return errors as a promise
+      return $q.reject(response);
+    };
+
+    return interceptorFactory;
+  });
